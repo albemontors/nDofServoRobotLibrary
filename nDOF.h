8c40a8dad4d7423fa5ee07dcc7b1db443a58c7c2
Alberto Montorsi <57532839+albemontors@users.noreply.github.com>
2022-10-20 12:28:02 +0100

    Mat4 class now have embedded ints
    Still need debugging

--- nDOF.h
+++ nDOF.h
@@ -21,18 +21,86 @@ Library for control of servo motors robotic systems
 
 class Mat4 {
     public:
+        /**
+        * @brief Constructor, makes a 0s 4x4 matrix
+        * @return this
+        */
         Mat4();
+        /**
+        * @brief Constructor, makes a 4x4 matrix starting from a square array
+        * @param b[4][4] has to be a square array
+        * @return this
+        */
         Mat4(float** b);
-        Mat4 write(Mat4 b);                                  
+        /**
+        * @brief writes the input into this
+        * @param matrix to write into this
+        * @return this
+        */
+        Mat4 write(Mat4 b);    
+        /**
+        * @brief row by column multiplication between this and param
+        * @param b second member of multiplication
+        * @return this
+        */                              
         Mat4 multiply(Mat4 b);
-        Mat4 generateIdentity();                           
-        Mat4 generateTrnX(float val);                                        //generate translation along X
-        Mat4 generateTrnZ(float val);                                        //generate translation along Z         
-        Mat4 generateRotX(float theta);                                        //generate rotation around X
-        Mat4 generateRotZ(float theta);                                       //generate rotation around Z
-        Mat4 multiply4DH(Mat4 TrZ, Mat4 RtZ, Mat4 RtX, Mat4 TrX);
+        /**
+        * @brief writes this with identity 4x4 matrix
+        * @return this
+        */
+        Mat4 generateIdentity(); 
+        /**
+         * @brief Writes the translation matrix around X axis on this
+         * @param val lenght of translation
+         * @return this
+         */                          
+        Mat4 generateTrnX(float val);
+        /**
+         * @brief Writes the translation matrix around Z axis on this
+         * @param val lenght of translation
+         * @return this
+         */                                            
+        Mat4 generateTrnZ(float val); 
+        /**
+         * @brief Writes the rotational matrix around X axis on this
+         * @param val angle in rads
+         * @return this
+         */                                                  
+        Mat4 generateRotX(float theta);
+        /**
+         * @brief Writes the rotational matrix around Z axis on this
+         * @param val angle in rads
+         * @return this
+         */                                           
+        Mat4 generateRotZ(float theta);
+        /**
+         * @brief multiplyes the 4 matrices in row and writes on this
+         * @param RtZ matrix of rotation around Z
+         * @param TrZ matrix of translation along Z
+         * @param TrX matrix of translation along X
+         * @param RtX matrix of rotation around X
+         * @return this
+         */                                          
+        Mat4 multiply4DH(Mat4 RtZ, Mat4 TrZ, Mat4 TrX, Mat4 RtX);
+        /**
+        * @brief reads a cell
+        * @param row number [0-3]
+        * @param column number [0-3]
+        * @return this
+        */
         float readCell(uint8_t row, uint8_t column);
+        /**
+        * @brief writes a cell
+        * @param row number [0-3]
+        * @param column number [0-3]
+        * @param value to write
+        * @return this
+        */
         Mat4 writeCell(uint8_t row, uint8_t column, float value);
+        /**
+        * @brief prints the matrix to terminal in a fancy layout
+        * @return this
+        */
         Mat4 verbose();
 
     private:
